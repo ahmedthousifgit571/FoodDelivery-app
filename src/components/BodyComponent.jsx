a82ce4ac6c8cfbb60a9713674200158fd3ee8ee9
ahmed thousif <ahmedthousif571@gmail.com>
2025-03-31 22:49:43 +0530

    search func over

--- src/components/BodyComponent.jsx
+++ src/components/BodyComponent.jsx
@@ -3,21 +3,33 @@ import RestaurantCard from "./RestaurantCard";
 import { restaurantList } from "../constants/api";
 
 
+function filterData(searchInput, restaurant) {
+  return restaurant.filter((resto) =>
+    resto.card.card.info.name.toLowerCase().includes(searchInput.toLowerCase())
+  );
+}
+
 function BodyComponent() {
-  const [searchInput,setSearchInput] = useState("KFC")
-  const [searchclicked,setSearClicked] = useState("false")
+  const [searchInput,setSearchInput] = useState("")
+  const [restaurant,setRestaurant] = useState(restaurantList)
+
+ 
   return (
     <>
       <div className="search-container">
         <input className="search-text" type="text" value={searchInput} onChange={(e) => setSearchInput(e.target.value)} />
-        <button className="search-input" onClick  ={()=>searchclicked==='false333'? setSearClicked("true") : set}>search</button>
-        <h1>{searchclicked}</h1>
+        <button className="search-input" onClick  ={()=> {
+          const data = filterData(searchInput,restaurant)
+          setRestaurant(data)
+          
+         }}>search</button>
+        
       </div>
       <div className="restaurant-list">
-        {restaurantList.map((restaurant, index) => (
+        {restaurant.map((resto) => (
           <RestaurantCard
-            key={restaurant.card.card.info.id}
-            restaurant={restaurant}
+            key={resto.card.card.info.id}
+            restaurant={resto}
           />
         ))}
       </div>
