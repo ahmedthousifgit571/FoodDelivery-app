7ae8070de89425dc412b2a3712cc2b7475ba77a2
ahmed thousif <ahmedthousif571@gmail.com>
2025-03-31 18:52:57 +0530

    search func

--- src/components/BodyComponent.jsx
+++ src/components/BodyComponent.jsx
@@ -1,20 +1,29 @@
-import React from 'react'
-import RestaurantCard from './RestaurantCard'
-import { restaurantList } from '../constants/api'
+import React, { useState } from "react";
+import RestaurantCard from "./RestaurantCard";
+import { restaurantList } from "../constants/api";
+
 
 function BodyComponent() {
-    
+  const [searchInput,setSearchInput] = useState("KFC")
+  const [searchclicked,setSearClicked] = useState("false")
   return (
-    <div className='restaurant-list'>
-      {
-        restaurantList.map((restaurant,index)=> 
-        <RestaurantCard key={restaurant.card.card.info.id} restaurant={restaurant}/>
-        )
-      }
-
-
-    </div>
-  )
+    <>
+      <div className="search-container">
+        <input className="search-text" type="text" value={searchInput} onChange={(e) => setSearchInput(e.target.value)} />
+        <button className="search-input" onClick  ={()=>searchclicked==='false333'? setSearClicked("true") : set}>search</button>
+        <h1>{searchclicked}</h1>
+      </div>
+      <div className="restaurant-list">
+        {restaurantList.map((restaurant, index) => (
+          <RestaurantCard
+            key={restaurant.card.card.info.id}
+            restaurant={restaurant}
+          />
+        ))}
+      </div>
+    </>
+  );
 }
 
-export default BodyComponent
+export default BodyComponent;
+
