5725d157bbbc250efbf7ca7bb93c24b7a0b1f8bd
ahmed thousif <ahmedthousif571@gmail.com>
2025-04-01 19:16:20 +0530

    shimmer ui

--- src/components/BodyComponent.jsx
+++ src/components/BodyComponent.jsx
@@ -1,25 +1,45 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 import { restaurantList } from "../constants/api";
+import Shimmer from "./Shimmer";
 
 
 function filterData(searchInput, restaurant) {
   return restaurant.filter((resto) =>
-    resto.card.card.info.name.toLowerCase().includes(searchInput.toLowerCase())
+    resto.info.name.toLowerCase().includes(searchInput.toLowerCase())
   );
 }
 
 function BodyComponent() {
   const [searchInput,setSearchInput] = useState("")
-  const [restaurant,setRestaurant] = useState(restaurantList)
+  const [restaurant,setRestaurant] = useState([])
+  const [allRestaurant, setAllRestaurant] = useState([]);
 
- 
-  return (
+  useEffect(()=>{
+    getRestaurants()
+  },[])
+
+  
+  async function getRestaurants(){
+    try{
+      const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=10.9214807&lng=77.004626&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+    const json = await data.json()
+    console.log(json)
+    setRestaurant(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants || [])
+    setAllRestaurant(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants || [])
+
+    console.log(json);
+    }catch(error){
+      console.error('error fetching data:',error)
+    }
+   
+  }
+  return (restaurant.length === 0 ) ? <Shimmer /> : (
     <>
       <div className="search-container">
         <input className="search-text" type="text" value={searchInput} onChange={(e) => setSearchInput(e.target.value)} />
         <button className="search-input" onClick  ={()=> {
-          const data = filterData(searchInput,restaurant)
+          const data = filterData(searchInput,allRestaurant)
           setRestaurant(data)
           
          }}>search</button>
@@ -28,7 +48,7 @@ function BodyComponent() {
       <div className="restaurant-list">
         {restaurant.map((resto) => (
           <RestaurantCard
-            key={resto.card.card.info.id}
+            key={resto.info.id}
             restaurant={resto}
           />
         ))}
---
+++ src/components/Shimmer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Shimmer() {
+  return (
+    <div>
+      <h1>Loading.....</h1>
+    </div>
+  )
+}
+
+export default Shimmer
