0c96ae6278918ab3e79c61771d071491950f33e2
ahmed thousif <ahmedthousif571@gmail.com>
2025-04-14 23:25:43 +0530

    redux store setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,23 @@
-
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 // import './App.css'
-import '../src/index.css'
-import HeaderComponent from './components/HeaderComponent'
-import FooterComponent from './components/FooterComponent'
-import { Outlet} from 'react-router-dom'
-
+import "../src/index.css";
+import HeaderComponent from "./components/HeaderComponent";
+import FooterComponent from "./components/FooterComponent";
+import { Outlet } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./utils/store";
 
 function App() {
-
- 
   return (
     <>
-    
-
-      <HeaderComponent />
-      <Outlet />
-      <FooterComponent />
-
-      
+      <Provider store={store}>
+        <HeaderComponent />
+        <Outlet />
+        <FooterComponent />
+      </Provider>
     </>
-  )
+  );
 }
 
-
-
-export default App
+export default App;
